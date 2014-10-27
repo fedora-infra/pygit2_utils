0d63a021aa28e568e1e0fb724067840c16885ede
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-27 16:02:57 +0100

    Add a GitConfig object that can be used to parse and load git configuration files
    This object can then be used to retrieve the proper user and email to
    give proper attribution when making a commit.

--- pygit2_utils/__init__.py
+++ pygit2_utils/__init__.py
@@ -12,10 +12,89 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
 import os
 import errno
 
 import pygit2
+
+
+class GitConfig(object):
+    """ Generic class to access the git configuration. """
+
+    def __init__(self, config=None, repopath=None):
+        """ Constructor of the GitConfig class.
+
+        :kwarg config: path to a git configuration file.
+        :type config: str
+        :kwarg repopath: path to a local git
+
+        """
+        if config is None:
+            if os.path.join('~', '.gitconfig'):
+                config = os.path.expanduser(
+                    os.path.join('~', '.gitconfig'))
+            else:
+                config = os.path.expanduser(
+                    os.path.join('~', '.config', 'git', 'config'))
+
+        configs = []
+        if config:
+            configs.append(config)
+        if repopath is not None:
+            configs.append(os.path.join(repopath, '.git', 'config'))
+
+        self.config = configparser.ConfigParser()
+        self.config.read(configs)
+
+    def get(self, section, option):
+        """ Returned a specfic option in a specified section.
+
+        ini file have the basic structure:
+            [section]
+            option = value
+        for example:
+            [user]
+            name = foo
+            email = foo@bar.com
+
+        With the present method you can do obj.get('user', 'name') to
+        retrieve `foo`.
+
+        :arg section: the name of the section to query
+        :type section: str
+        :arg option: the name of the option to retrieve in the specified
+            section
+        :type section: str
+        :return: the value of the option in the specified section
+        :rtype: str or int
+        :raises configparser.NoSectionError: if the section asked could not
+            be found
+        :raises configparser.NoOptionError: if the option asked could not
+            be found in the specified section
+
+        """
+        return self.config.get(section, option)
+
+    def get_user(self):
+        """ Utility method to retrieve the user as desired for a commit in
+        git.
+        For example will return: foo <foo@bar.com>
+
+        :return: the string of the user and his/her email as desired for a
+            commit in git
+        :rtype: str
+
+        """
+        tmpl = '%(name)s <%(email)s>'
+        return tmpl % dict(
+            name=self.get('user', 'name'),
+            email=self.get('user', 'email')
+        )
 
 
 class GitRepo(object):
