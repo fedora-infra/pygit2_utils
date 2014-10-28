841bf90082191aa72e20adf3af9b3e743be8b97b
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-28 16:47:09 +0100

    Increase the unit-test coverage for the GitRepo().commit() method

--- tests/test_scm.py
+++ tests/test_scm.py
@@ -174,7 +174,31 @@ class ScmTests(BaseTests):
         with open(os.path.join(repo_path, 'sources'), 'w') as stream:
             stream.write('\nBoo!!2')
 
+        # Commit with a list of (one) file
         commitid = repo.commit('Commit from the tests', ['sources'])
+
+        # Check that the commitid returned has an .hex attribute that is a
+        # string
+        self.assertTrue(isinstance(commitid.hex, str))
+
+        # Check the latest commit has the same hash as the commitid returned
+        self.assertEqual(
+            commitid.hex,
+            repo_obj.revparse_single('HEAD').oid.hex
+        )
+
+        # Check the information of the latest commit
+        commit = repo_obj.get(repo_obj.revparse_single('HEAD').oid.hex)
+
+        self.assertEqual(commit.message, 'Commit from the tests')
+        self.assertEqual(commit.author.name, 'foo')
+        self.assertEqual(commit.author.email, 'foo@bar.com')
+
+        with open(os.path.join(repo_path, 'sources'), 'w') as stream:
+            stream.write('\nBoo!!2')
+
+        # Commit with a single file
+        commitid = repo.commit('Commit from the tests', 'sources')
 
         # Check that the commitid returned has an .hex attribute that is a
         # string
