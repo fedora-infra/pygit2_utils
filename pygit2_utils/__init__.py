7b877c6087d5e98bc20e7e609283e68cee36b430
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-28 12:03:29 +0100

    Drop the GitConfig class in favor of pygit2's handling of git configuration

--- pygit2_utils/__init__.py
+++ pygit2_utils/__init__.py
@@ -23,80 +23,6 @@ import errno
 import pygit2
 
 
-class GitConfig(object):
-    """ Generic class to access the git configuration. """
-
-    def __init__(self, config=None, repopath=None):
-        """ Constructor of the GitConfig class.
-
-        :kwarg config: path to a git configuration file.
-        :type config: str
-        :kwarg repopath: path to a local git
-
-        """
-        if config is None:
-            if os.path.join('~', '.gitconfig'):
-                config = os.path.expanduser(
-                    os.path.join('~', '.gitconfig'))
-            else:
-                config = os.path.expanduser(
-                    os.path.join('~', '.config', 'git', 'config'))
-
-        configs = []
-        if config:
-            configs.append(config)
-        if repopath is not None:
-            configs.append(os.path.join(repopath, '.git', 'config'))
-
-        self.config = configparser.ConfigParser()
-        self.config.read(configs)
-
-    def get(self, section, option):
-        """ Returned a specfic option in a specified section.
-
-        ini file have the basic structure:
-            [section]
-            option = value
-        for example:
-            [user]
-            name = foo
-            email = foo@bar.com
-
-        With the present method you can do obj.get('user', 'name') to
-        retrieve `foo`.
-
-        :arg section: the name of the section to query
-        :type section: str
-        :arg option: the name of the option to retrieve in the specified
-            section
-        :type section: str
-        :return: the value of the option in the specified section
-        :rtype: str or int
-        :raises configparser.NoSectionError: if the section asked could not
-            be found
-        :raises configparser.NoOptionError: if the option asked could not
-            be found in the specified section
-
-        """
-        return self.config.get(section, option)
-
-    def get_user(self):
-        """ Utility method to retrieve the user as desired for a commit in
-        git.
-        For example will return: foo <foo@bar.com>
-
-        :return: the string of the user and his/her email as desired for a
-            commit in git
-        :rtype: str
-
-        """
-        tmpl = '%(name)s <%(email)s>'
-        return tmpl % dict(
-            name=self.get('user', 'name'),
-            email=self.get('user', 'email')
-        )
-
-
 class GitRepo(object):
 
     def __init__(self, path):
@@ -108,6 +34,12 @@ class GitRepo(object):
         """
         self.path = path
         self.repository = pygit2.Repository(self.path)
+        self.config = self.repository.config
+
+        # If there is a local config, use it
+        potential_config = os.path.join(self.path, '.git', 'config')
+        if os.path.isfile(potential_config):
+            self.config.add_file(potential_config)
 
     @classmethod
     def clone_repo(cls, url, dest_path):
@@ -197,8 +129,6 @@ class GitRepo(object):
         if not isinstance(files, list):
             files = [files]
 
-        config = GitConfig(repopath=self.path)
-
         for filename in files:
             self.repository.index.add(filename)
         self.repository.index.write()
@@ -206,9 +136,9 @@ class GitRepo(object):
 
         # Set variables needed for the commit
         author = pygit2.Signature(
-            config.get('user', 'name'), config.get('user', 'email'))
-        committer = pygit2.Signature(
-            config.get('user', 'name'), config.get('user', 'email'))
+            self.config.get_multivar('user.name')[0],
+            self.config.get_multivar('user.email')[0],
+        )
         parent = self.repository.revparse_single('HEAD').oid.hex
 
         # Do the commit
@@ -216,7 +146,7 @@ class GitRepo(object):
             # the name of the reference to update
             self.repository.head.name,
             author,
-            committer,
+            author,
             message,
             # binary string representing the tree object ID
             tree,
