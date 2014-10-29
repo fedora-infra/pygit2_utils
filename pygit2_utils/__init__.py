de3d6940359079095f446690821e916dc9b70770
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-29 11:52:19 +0100

    Drop un-used imports

--- pygit2_utils/__init__.py
+++ pygit2_utils/__init__.py
@@ -12,10 +12,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-try:
-    import configparser
-except ImportError:
-    import ConfigParser as configparser
 
 import os
 import errno
